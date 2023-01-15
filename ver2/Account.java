faa9e4b80f0b42f41ee3499446964a70d30cdc83
xDylan03x <102174075+xDylan03x@users.noreply.github.com>
2023-01-15 17:26:34 -0500

    Added new comment to the init var "balance"

--- ver2/Account.java
+++ ver2/Account.java
@@ -2,6 +2,7 @@ package ver2;
 
 public class Account {
     // Instance variable, only available inside this class.
+    // A comment for the GitHub Lab
     private double balance;
     private String name;
 
